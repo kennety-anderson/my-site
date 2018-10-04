c1f536714e5f942e522f85f467a32444847f4798
kennety-anderson <kennetyg@gmail.com>
2018-10-04 01:32:53 -0300

    Criação do efeito maquina de escrever

--- arquivo.js
+++ arquivo.js
@@ -2,10 +2,28 @@
 (function(window, document) {
     'use strict';
 
+    //efeito maquina de escrever 
+    //declaração das constantes usadas
+    const titulo = document.querySelector('#header');
+    const message = "<Kennnety Anderson Front-end Programming, Web Design/>";
+    
+    //function para dividir a const utilizando um looṕ para acrecenter 
+    // cada caracter a cada 75 milisegundo vezes o numero do index
+    function typeWrite(elemento){
+        const current = message.split('');
+        elemento.innerHTML = "";
+        current.forEach(function(item, index){
+            setTimeout(function(){
+                elemento.innerHTML += item;
+            }, 75 * index); 
+        });
+    };
+
+    //invocação da função
+    typeWrite(titulo);
 
 
-
-
+    //efeito de escroll invisivel
     //Declaração de constantente para capturar o atributo dat-anime.
     const target = document.querySelectorAll('[data-anime]');
 
@@ -26,13 +44,12 @@
 
     //Chamada da função pelo menos uma vez.
     animeScroll();
-
+    
     //Ativação da funçao de scroll a partir do evento scroll.
     if (target.length) {
         window.addEventListener("scroll", function() {
             animeScroll();
         }, false)
     };
-
 
 })(window, document);
