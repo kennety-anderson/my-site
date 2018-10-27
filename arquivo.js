ea13e87a64bd1cb58acae447c5625cf5a7f5b14c
kennety-anderson <kennetyg@gmail.com>
2018-10-26 23:54:50 -0300

    Atualização de acordo com es6 e criação de duas colunas na area sobre do html

--- arquivo.js
+++ arquivo.js
@@ -4,20 +4,23 @@
 
     //efeito maquina de escrever 
     //declaração das constantes usadas
-    const titulo = document.querySelector('#header');
+    const titulo = document.querySelector('[data-header]');
     const message = "<Kennnety Anderson Front-end Programming, Web Design/>";
-    
-    //function para dividir a const utilizando um looṕ para acrecenter 
+
+    //function para dividir a const utilizando um loop para acrecenta  r 
     // cada caracter a cada 75 milisegundo vezes o numero do index
-    function typeWrite(elemento){
-        const current = message.split('');
-        elemento.innerHTML = "";
-        current.forEach(function(item, index){
-            setTimeout(function(){
-                elemento.innerHTML += item;
-            }, 75 * index); 
-        });
+    function typeWrite(elemento) {
+        //encadeamento de Array.from com forEach
+        // para dividir a message e gerar um loop com seus itens e index
+        //com o uso de uma arrow function
+        Array.from(message)
+            .forEach((item, index) => {
+                //Função que ira acrecentar cada elemento do array * o index
+                //a cada 75 milissegundos
+                setTimeout(() => elemento.innerHTML += item, 75 * index);
+            });
     };
+
 
     //invocação da função
     typeWrite(titulo);
@@ -32,10 +35,15 @@
 
     //Função de animação do scroll a cada 3 terços da tela comparada ao objeto window.
     function animeScroll() {
+        //Armazena dentro de uma const o valor do eixo y vertical somado ao tamanho da janela
+        //multiplicando por 3 e dividindo por 4 para pegar cerca de um terço do espaço da tela
         const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-        target.forEach(function(element) {
-            if (windowTop > element.offsetTop) {
+        target.forEach(element => {
+            //teste para verificar se windowTop é maior que a distancia especificada
+            //para adicionar o elemeto do setado pelo efeito scroll
+            if ((windowTop) > element.offsetTop) {
                 element.classList.add(animateClass);
+                //se não for maior ele remove o eelemeto
             } else {
                 element.classList.remove(animateClass);
             }
@@ -44,12 +52,9 @@
 
     //Chamada da função pelo menos uma vez.
     animeScroll();
-    
-    //Ativação da funçao de scroll a partir do evento scroll.
+    //Ativação da funçao com arrow function a partir do evento scroll.
     if (target.length) {
-        window.addEventListener("scroll", function() {
-            animeScroll();
-        }, false)
+        window.addEventListener("scroll", () => animeScroll());
     };
 
 })(window, document);
